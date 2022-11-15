b39e80d370c0807973198eb9c3dec49c3eaf6f50
Del <Del@WIN11>
2022-11-15 04:54:40 +0000

    Add project files.

---
+++ ToLowercase (CS)/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Crouchie.ToLowercase.CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button4 = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Button4
+            // 
+            this.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button4.Location = new System.Drawing.Point(659, 325);
+            this.Button4.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(164, 45);
+            this.Button4.TabIndex = 15;
+            this.Button4.Text = "Copy to Clipboard";
+            this.Button4.UseVisualStyleBackColor = true;
+            this.Button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Button3
+            // 
+            this.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button3.Location = new System.Drawing.Point(659, 530);
+            this.Button3.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(164, 45);
+            this.Button3.TabIndex = 14;
+            this.Button3.Text = "Clear";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button2.Location = new System.Drawing.Point(659, 612);
+            this.Button2.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(164, 45);
+            this.Button2.TabIndex = 13;
+            this.Button2.Text = "Exit";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Button1
+            // 
+            this.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button1.Location = new System.Drawing.Point(659, 47);
+            this.Button1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(164, 45);
+            this.Button1.TabIndex = 12;
+            this.Button1.Text = "Convert";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(11, 348);
+            this.Label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(120, 20);
+            this.Label2.TabIndex = 11;
+            this.Label2.Text = "Converted Text:";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(11, 20);
+            this.Label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(124, 20);
+            this.Label1.TabIndex = 10;
+            this.Label1.Text = "Text To Convert:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 376);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(620, 281);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 47);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(620, 281);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 677);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ToLowercase (C#)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button Button4;
+        internal System.Windows.Forms.Button Button3;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox textBox2;
+        internal System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ToLowercase (CS)/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Crouchie.ToLowercase.CS
+{
+    /// <summary>
+    /// This program is provided AS-IS and allows the user to 
+    /// quickly paste in text to be converted to lowercase and 
+    /// copy to the clipboard.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Constants
+        public const int WM_QUERYENDSESSION = 0x11;
+        #endregion
+
+        #region Convert
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text.ToLower().Trim();
+        }
+        #endregion
+
+        #region Exit
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion
+
+        #region Clear
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+            textBox1.Focus();
+        }
+        #endregion
+
+        #region Copy to Clipboard
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox2.Text.Trim()))
+            {
+                Clipboard.SetText(textBox2.Text.Trim());
+            }
+
+        }
+        #endregion
+
+        #region WndProc
+        protected override void WndProc(ref System.Windows.Forms.Message m)
+        {
+            if (m.Msg == WM_QUERYENDSESSION)
+                Application.Exit();
+            base.WndProc(ref m);
+        }
+        #endregion
+
+    }
+}
